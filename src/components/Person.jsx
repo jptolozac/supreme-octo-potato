770c0499ffec23af1cafef578d987cceb0595624
jptolozac <jeanpaultoloza@gmail.com>
2024-06-04 18:30:05 -0500

    Add: final changes

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,13 +1,19 @@
-export function Person({ name, role, img }){
-    return (
-        <div className="p-4 bg-secondary me-4 rounded">.
-            <div className="mx-auto">
-                <img src={img} alt={`Imagen de ${name}`} />
-            </div>
-            <div className="text-light mt-2">
-                <p className="fs-4">{name}</p>
-                <p>{role}</p>
-            </div>
-        </div>
-    )
+import React from 'react'
+
+export const Person = ({ id, name, role, img, handleEdit, handleDelete }) => {
+  return (
+    <div className="col">
+      <div className="card" style={{width: "18rem"}}>
+        <img src={img} className="card-img-top" alt={name}/>
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">{role}</p>
+          </div>
+          <div className='mb-4'>
+            <button className="btn btn-success me-2" onClick={handleEdit}>Editar</button>
+            <button className="btn btn-danger" onClick={() => handleDelete(id)} data-bs-toggle="modal" data-bs-target="#deleteModal" >Eliminar</button>
+          </div>
+      </div>
+    </div>
+  )
 }
